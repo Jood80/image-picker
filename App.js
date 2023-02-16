eabf6c09ae47719bf2b0fb04e9e7cee2dff7b01a
Jood80 <shawar.nujood@gmail.com>
2023-02-16 20:30:13 +0200

    add ErrorBoundaries, ImagePicker,

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import * as ImagePicker from 'expo-image-picker';
+import ErrorBoundary from 'react-native-error-boundary'
 
-export default function App() {
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+const PlaceholderImage = require('./assets/images/background-image.png');
+
+const ErrorFallback = (props) => (
+  <View style={styles.container}>
+    <Text style={styles.title}>Something happened!</Text>
+    <Text style={styles.text}>{props.error.toString()}</Text>
+  </View>
+)
+
+export default function App () {
+  const [selectedImage, setSelectedImage] = useState(null)
+
+  const pickImageAsync = async () => {
+    console.log("guess who arrived");
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1
+    })
+
+    if(!result.canceled) {
+      setSelectedImage(result.assets[0].uri)
+    } else {
+      alert("you did not select an image")
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <ImageViewer placeholderImgSrc={PlaceholderImage}
+            selectedImage={selectedImage} />
+        </View>
+
+        <View>
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+          <Button label="Use this photo" />
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </ErrorBoundary>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  footerContainer: {
+    alignItems: 'center',
+    flex: 1 / 3,
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 60,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  text: {
+    marginVertical: 16
+  }
 });
