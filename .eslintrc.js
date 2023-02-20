6dce8699813675c9580adfbb77492890ca0d8bfc
Jood80 <shawar.nujood@gmail.com>
2023-02-20 11:00:44 +0200

    fix: "''React' must be in scope when using JSX react native"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community', 'react-app'],
+  extends: ['@react-native-community'],
+  plugins: ['prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
 };
