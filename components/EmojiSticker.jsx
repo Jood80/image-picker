ff78f0c195b3f737969c1a65267aab742e837999
Jood80 <shawar.nujood@gmail.com>
2023-02-20 11:02:35 +0200

    chore: apply animation on emoji stickers

--- components/EmojiSticker.jsx
+++ components/EmojiSticker.jsx
@@ -7,7 +7,7 @@ const AnimatedImage = Animated.createAnimatedComponent(Image);
 export default function EmojiSticker({ ImageSize, stickerSrc }) {
   return (
     <View style={{ top: -350 }}>
-      <Image
+      <AnimatedImage
         source={stickerSrc}
         resizeMode="contain"
         style={{ width: ImageSize, height: ImageSize }}
