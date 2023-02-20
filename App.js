7381cf0fe0830687939964ee8e04cd9d8e201ad7
Jood80 <shawar.nujood@gmail.com>
2023-02-20 14:16:41 +0200

    feat: take screenshots via phone

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import * as MediaLibrary from 'expo-media-library';
 import { StyleSheet, View, Text, SafeAreaView } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import * as ImagePicker from 'expo-image-picker';
 import ErrorBoundary from 'react-native-error-boundary';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { captureRef } from 'react-native-view-shot';
 
 import {
   Button,
@@ -25,10 +27,12 @@ const ErrorFallback = (props) => (
 );
 
 export default function App() {
+  const [status, requestPermission] = MediaLibrary.usePermissions();
   const [selectedImage, setSelectedImage] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [showAppOptions, setShowAppOptions] = useState(false);
   const [pickedEmoji, setPickedEmoji] = useState(null);
+  const imageRef = useRef();
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -53,24 +57,44 @@ export default function App() {
     setIsModalVisible(true);
   };
 
-  const onSaveImageAsync = async () => {};
+  const onSaveImageAsync = async () => {
+    try {
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1,
+      });
+      console.log(localUri);
+      await MediaLibrary.saveToLibraryAsync(localUri);
+      if (localUri) {
+        alert('Saved!');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const onModalClose = () => {
     setIsModalVisible(false);
   };
+
+  if (status === null) {
+    requestPermission();
+  }
 
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
       <GestureHandlerRootView style={styles.container}>
         <SafeAreaView style={styles.safeArea}>
           <View style={styles.imageContainer}>
-            <ImageViewer
-              placeholderImgSrc={PlaceholderImage}
-              selectedImage={selectedImage}
-            />
-            {pickedEmoji && (
-              <EmojiSticker ImageSize={40} stickerSrc={pickedEmoji} />
-            )}
+            <View ref={imageRef} collapsable={false}>
+              <ImageViewer
+                placeholderImgSrc={PlaceholderImage}
+                selectedImage={selectedImage}
+              />
+              {pickedEmoji && (
+                <EmojiSticker ImageSize={40} stickerSrc={pickedEmoji} />
+              )}
+            </View>
           </View>
 
           {showAppOptions ? (
@@ -125,6 +149,7 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingTop: 60,
+    marginHorizontal: 20,
   },
   optionsContainer: {
     position: 'absolute',
